74bf1397be8670e1a73596ce487c79df7bfc087f
Jayanth <jayanth.kurmeti@gmail.com>
2025-04-30 15:07:31 +0530

    Fix: Add timestamps to Note schema

--- server/models/Note.js
+++ server/models/Note.js
@@ -1,20 +1,17 @@
 const mongoose = require('mongoose');
 
-const noteSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true } 
+);
 
-const Note = mongoose.model('Note', noteSchema);
-
-module.exports = Note;
+module.exports = mongoose.model('Note', noteSchema);
